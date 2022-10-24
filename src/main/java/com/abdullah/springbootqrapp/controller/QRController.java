ba16a3204234d96fee2f817f2d3b29c82b0443b7
Abdullah Bakış <abdullahbakis1672@gmail.com>
2022-10-24 07:27:43 +0300

    Update

--- src/main/java/com/abdullah/springbootqrapp/controller/QRController.java
+++ src/main/java/com/abdullah/springbootqrapp/controller/QRController.java
@@ -1,2 +1,33 @@
-package com.abdullah.springbootqrapp.controller;public class QRController {
+package com.abdullah.springbootqrapp.controller;
+
+import com.abdullah.springbootqrapp.QRCodeGenerator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QRController {
+
+    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QRCode.png";
+
+
+    @GetMapping(value = "/generateAndDownloadQRCode/{codeText}/{width}/{height}")
+    public void download(
+            @PathVariable("codeText") String codeText,
+            @PathVariable("width") Integer width,
+            @PathVariable("height") Integer height)
+            throws Exception {
+        QRCodeGenerator.generateQRCodeImage(codeText, width, height, QR_CODE_IMAGE_PATH);
+    }
+
+    @GetMapping(value = "/generateQRCode/{codeText}/{width}/{height}")
+    public ResponseEntity<byte[]> generateQRCode(
+            @PathVariable("codeText") String codeText,
+            @PathVariable("width") Integer width,
+            @PathVariable("height") Integer height)
+            throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(QRCodeGenerator.getQRCodeImage(codeText, width, height));
+    }
 }
